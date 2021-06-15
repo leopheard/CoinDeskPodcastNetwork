cb7265ef399c80018a4c754bd5498647251c5ff7
leopheard <leopheard.misc@gmail.com>
2021-06-14 20:45:19 -0400

    upload

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-<a href="kodi.tv">KODI<a> (XBMC/OSMC/Firestick) audio addon for the <a href="https://art19.com/shows/late-confirmation">CoinDesk Podcast Network</a>.<br>
+<a href="https://kodi.tv">KODI<a> (XBMC/OSMC/Firestick) audio addon for the <a href="https://art19.com/shows/late-confirmation">CoinDesk Podcast Network</a>.<br>
 
-<img src="https://content.production.cdn.art19.com/images/90/02/1e/e5/90021ee5-ee71-49ec-8dee-4bb74febbe02/6da5837f399bae77fa0e90f94daae54bc1175ea0550ca8f8c388e82efd3349d9e89441d7c9474249e80a216b2f2a9f9a0cf722f8ff53304c06aa16804669ef05.jpeg"><br>
+<img src="https://content.production.cdn.art19.com/images/90/02/1e/e5/90021ee5-ee71-49ec-8dee-4bb74febbe02/6da5837f399bae77fa0e90f94daae54bc1175ea0550ca8f8c388e82efd3349d9e89441d7c9474249e80a216b2f2a9f9a0cf722f8ff53304c06aa16804669ef05.jpeg"width="400" height="400" alt="Coindesk podcast"><br>
 
 The top stories and best shows in the blockchain world, delivered daily from the team at CoinDesk.<br>
 
-<a href="www.kodi.tv">Kodi</a> (formerly known as XBMC) is an award-winning free and open source (GPL) software media player and entertainment hub that can be installed on Linux, OSX, Windows, iOS and Android, featuring a 10-foot user interface for use with televisions and remote controls. It allows users to play and view most videos, music, podcasts, and other digital media files from local and network storage media and the internet.<br>
+<a href="https://www.kodi.tv">Kodi</a> (formerly known as XBMC) is an award-winning free and open source (GPL) software media player and entertainment hub that can be installed on Linux, OSX, Windows, iOS and Android, featuring a 10-foot user interface for use with televisions and remote controls. It allows users to play and view most videos, music, podcasts, and other digital media files from local and network storage media and the internet.<br>
 
-<b>^^^^ To install this addon ^^^^</b>, either use the <a href="https://www.tvaddons.co/github-browser-kodi/">Kodi Github installer</a> addon or save the .zip file downloaded from the 'clone or download' button (on this page above) to somewhere the Kodi can access (e.g. network drive or USB stick). Then on the Kodi, go to addons > install from zip file.<br>
+<b>^^^^ To install this addon ^^^^</b>, either use the <a href="https://www.tvaddons.co/github-browser-kodi/">Kodi Github installer</a> addon or save the .zip file downloaded from the Code > 'Download Zip' button (on this page above) to somewhere the Kodi can access (e.g. network drive or USB stick). Then on the Kodi, go to addons > install from zip file.<br>
 
-<br><a href="http://www.kodi.tv"><img src="https://kodi.tv/sites/default/files/page/field_image/about--devices.jpg">
+<br><a href="https://www.kodi.tv"><img src="https://github.com/leopheard/Audio-Podcasts/blob/master/resources/media/about--devices.jpg?raw=true">
--- addon.py
+++ addon.py
@@ -3,6 +3,12 @@ from resources.lib import mainaddon
 
 plugin = Plugin()
 url1 = "https://rss.art19.com/late-confirmation"
+url2 = "https://rss.art19.com/coindesk-reports"
+url3 = "https://rss.art19.com/markets-daily"
+url4 = "https://rss.art19.com/coindesks-money-reimagined"
+url5 = "https://rss.art19.com/mapping-out-eth-20"
+url6 = "https://rss.art19.com/hard-problems-with-bram-cohen"
+
 @plugin.route('/')
 def main_menu():
     items = [
@@ -11,9 +17,25 @@ def main_menu():
             'path': plugin.url_for('episodes1'),
             'thumbnail': "https://content.production.cdn.art19.com/images/90/02/1e/e5/90021ee5-ee71-49ec-8dee-4bb74febbe02/6da5837f399bae77fa0e90f94daae54bc1175ea0550ca8f8c388e82efd3349d9e89441d7c9474249e80a216b2f2a9f9a0cf722f8ff53304c06aa16804669ef05.jpeg"},
         {
-            'label': plugin.get_string(30000),
-            'path': plugin.url_for('episodes'),
+            'label': plugin.get_string(30002),
+            'path': plugin.url_for('episodes2'),
             'thumbnail': "https://content.production.cdn.art19.com/images/90/02/1e/e5/90021ee5-ee71-49ec-8dee-4bb74febbe02/6da5837f399bae77fa0e90f94daae54bc1175ea0550ca8f8c388e82efd3349d9e89441d7c9474249e80a216b2f2a9f9a0cf722f8ff53304c06aa16804669ef05.jpeg"},
+        {
+            'label': plugin.get_string(30003), 
+            'path': plugin.url_for('episodes3'),
+            'thumbnail': "https://static.coindesk.com/wp-content/uploads/2020/09/podcast-cover-3fa4c53c5e702bca96b8100b1e1e821b-400x400.jpeg"},
+        {
+            'label': plugin.get_string(30004), 
+            'path': plugin.url_for('episodes4'),
+            'thumbnail': "https://static.coindesk.com/wp-content/uploads/2020/10/podcast-cover-19a5b4f9dd7641000db1844bc66413b9-400x400.jpeg?format=webp"},
+        {
+            'label': plugin.get_string(30005), 
+            'path': plugin.url_for('episodes5'),
+            'thumbnail': "https://static.coindesk.com/wp-content/uploads/2021/02/MOE_3000x3000_v6-400x400.jpg?format=webp"},
+        {
+            'label': plugin.get_string(30006),
+            'path': plugin.url_for('episodes6'),
+            'thumbnail': "https://static.coindesk.com/wp-content/uploads/2021/05/podcast-cover-745cc4af1412b3a27c30bc3d5a21e035-400x400.jpeg?format=webp"},
     ]
     return items
 
@@ -24,11 +46,35 @@ def episodes1():
     items = mainaddon.compile_playable_podcast1(playable_podcast1)
     return items
 
-@plugin.route('/episodes/')
-def episodes():
-    soup1 = mainaddon.get_soup1(url1)
-    playable_podcast = mainaddon.get_playable_podcast(soup1)
-    items = mainaddon.compile_playable_podcast(playable_podcast)
+@plugin.route('/episodes2/')
+def episodes2():
+    soup2 = mainaddon.get_soup2(url2)
+    playable_podcast2 = mainaddon.get_playable_podcast2(soup2)
+    items = mainaddon.compile_playable_podcast2(playable_podcast2)
+    return items
+@plugin.route('/episodes3/')
+def episodes3():
+    soup3 = mainaddon.get_soup3(url3)
+    playable_podcast3 = mainaddon.get_playable_podcast3(soup3)
+    items = mainaddon.compile_playable_podcast3(playable_podcast3)
+    return items
+@plugin.route('/episodes4/')
+def episodes4():
+    soup4 = mainaddon.get_soup4(url4)   
+    playable_podcast4 = mainaddon.get_playable_podcast4(soup4)
+    items = mainaddon.compile_playable_podcast4(playable_podcast4)
+    return items
+@plugin.route('/episodes5/')
+def episodes5():
+    soup5 = mainaddon.get_soup5(url5)
+    playable_podcast5 = mainaddon.get_playable_podcast5(soup5)
+    items = mainaddon.compile_playable_podcast5(playable_podcast5)
+    return items
+@plugin.route('/episodes6/')
+def episodes6():
+    soup6 = mainaddon.get_soup6(url6)
+    playable_podcast6 = mainaddon.get_playable_podcast6(soup6)
+    items = mainaddon.compile_playable_podcast6(playable_podcast6)
     return items
 
 if __name__ == '__main__':
